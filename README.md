6ad81ef506c124799fb61294a138511def1c611d
Lia <liaforweb@gmail.com>
2020-03-18 16:54:21 +0200

    desctop

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 2020-03-stolyar
+
+
+<a href="https://lia5.github.io/2020-03-stolyar/myapp">main</a>
+
---
+++ myapp/js/common.js
@@ -0,0 +1,305 @@
+$(function() {
+    if(jQuery('.scroll-to').length) {
+        var $page = $('html, body');
+        $('.scroll-to[href*="#"]').click(function() {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 400);
+            return false;
+        });
+    }
+	// Custom JS
+    $('.products__items').owlCarousel({
+        loop: true,
+        dots: false,
+        margin: 10,
+        nav: true,
+        navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+    $('.idea__items').owlCarousel({
+        loop: false,
+        dots: false,
+        margin: 10,
+        nav: true,
+        navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    //Делегируем события кнопок next prev по умолчанию нашим кнопкам, которые могут находится ыне контейнера слайдера
+	var owl=$(".products__items.owl-carousel");
+	owl.owlCarousel();
+	//$(".next") - находим нашу кнопку
+	$(".next").click(function(){
+		owl.trigger("next.owl.carousel");
+	});
+	$(".prev").click(function(){
+		owl.trigger("prev.owl.carousel");
+	});
+
+
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+
+      $('.institutions__slider').owlCarousel({
+        loop: false,
+        dots: false,
+        margin: 40,
+        nav: false,
+        // autoWidth:true,
+        // navText: ['<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>', '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve"><metadata> Svg Vector Icons : http://www.onlinewebfonts.com/icon </metadata><g><g transform="matrix(1 0 0 -1 0 1008)"><path d="M756.2,741.8L990,508L756.2,274.2l-27,27L918.1,490H10v36h908.1L729.3,714.8L756.2,741.8z"/></g></g></svg>'],
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
+    //select-number form
+    if(jQuery('.phone-mask').length) {
+        jQuery(function($){
+            $(".phone-mask").mask("+38(999) 999-9999");
+        });
+    }
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+        }
+        return(false);
+    }
+
+    $('form').find('input.utm_source').each(function() {
+        var a = getQueryVariable('utm_source');
+        if(a){
+            $(this).val(a);
+        }
+    }); 
+    $('form').find('input.utm_medium').each(function() {
+        var a = getQueryVariable('utm_medium');
+        if(a){
+            $(this).val(a);
+        }
+    });
+    $('form').find('input.utm_campaign').each(function() {
+        var a = getQueryVariable('utm_campaign');
+        if(a){
+            $(this).val(a);
+        }
+    });
+    $('form').find('input.utm_term').each(function() {
+        var a = getQueryVariable('utm_term');
+        if(a){
+            $(this).val(a);
+        }
+    });
+    $('form').find('input.utm_content').each(function() {
+        var a = getQueryVariable('utm_content');
+        if(a){
+            $(this).val(a);
+        }
+    });
+    
+    if(jQuery('.modal__wrap').length) {
+        let modalWrap = $('.modal__wrap');
+        
+        //popup
+        $(".modal-open").click(function (e){
+          e.preventDefault();
+          var numModal = $(this).attr('href');
+          var modal =  $(numModal);
+          modalWrap.removeClass('fadeOutUp');
+          modalWrap.addClass('fadeInDown');
+          modal.removeClass('disabled');
+          modal.addClass('flex');
+          $('body').addClass('body-modal-open');
+          // body.addClass('body-modal');
+        });
+      
+        // $(".modal-open").bind('touchstart', function(e){
+        //   e.preventDefault();
+        //   var numModal = $(this).attr('href');
+        //   var modal =  $(numModal);
+        //   modalWrap.removeClass('fadeOutUp');
+        //   modalWrap.addClass('fadeInDown');
+        //   modal.removeClass('disabled');
+        //   modal.addClass('flex');
+        //   // body.addClass('body-modal');
+        // });
+      
+      
+        $('.modal-close').click(function (){
+      
+          modalWrap.removeClass('fadeInDown');
+          modalWrap.addClass('fadeOutUp');
+          setTimeout(function() {
+              $('.modal').addClass('disabled');
+            }, 700);
+          setTimeout(function() {
+              $('.modal').removeClass('flex');
+              $('body').removeClass('body-modal-open');
+            }, 800);  
+      
+        });
+      
+        $('.modal').mouseup(function (e){ // событие клика по веб-документу
+          var div = $(".modal__body"); // тут указываем ID элемента
+          var close = $('.modal-close');
+          if (close.is(e.target)) {
+      
+          } else if (!div.is(e.target) // если клик был не по нашему блоку
+          && div.has(e.target).length === 0) { // и не по его дочерним элементам
+              var modalWrap = $('.modal__wrap');
+              modalWrap.removeClass('fadeInDown');
+              modalWrap.addClass('fadeOutUp');
+              setTimeout(function() {
+                  $('.modal').addClass('disabled');
+              }, 700);
+              setTimeout(function() {
+                  $('.modal').removeClass('flex');
+                  $('body').removeClass('body-modal-open');
+              }, 800); 
+            
+          }
+        });
+      }
+
+    //click on form submit button - AMO
+    $('.kviz__btn').on('click', function(e){
+        e.preventDefault();
+        var btn = $(this);
+        $($(this).parent().parent().parent()).each(function () {
+            var form = $(this);
+            form.find('.rfield').addClass('empty_field');
+
+                // Функция проверки полей формы
+
+                form.find('.rfield').each(function(){
+                if($(this).val() != ''){
+                    // Если поле не пустое удаляем класс-указание
+                    $(this).removeClass('empty_field');
+
+                if (!form.find('.empty_field').length) {
+                    var numModal = btn.attr('href');
+                    var modal =  $(numModal);
+                    var modalWrap = $('.modal__wrap');
+                    modalWrap.removeClass('fadeOutUp');
+                    modalWrap.addClass('fadeInDown');
+                    $('.modal').addClass('disabled');
+                    modal.removeClass('disabled');
+                    modal.addClass('flex');
+                    $('body').addClass('body-modal-open');
+                    console.log('form');
+                    form2 = form.closest('form');
+                    jQuery.ajax({
+                        method: "POST",
+                        data: form2.serialize(),
+                        // url: quizAjax.url,
+                        url: '../sendamo.php',
+                        dataType: "json",
+                        success: function (json) {
+                            // if (json.success) {
+                                // jQuery(".wizard-section").fadeOut(100);
+                                window.location.href = "/quiz-thanks/";
+                            // }
+                        }
+                    });
+                    $(this).attr('href', "#").removeClass('modal-open').removeClass('kviz__btn').css('pointer-events', 'none');
+                    $(this).parent().css('opacity', '0.5').css('pointer-events', 'none');
+                    fbq('track', 'Lead');
+                    }
+                // fbq('track', 'Lead');
+
+                } else {}
+            });
+        })
+    });
+    if(jQuery('.scroll-to').length) {
+        var $page = $('html, body');
+        $('.scroll-to[href*="#"]').click(function() {
+            $page.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 400);
+            return false;
+        });
+    }
+
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+//menu
+    var menu = document.querySelector('.menu-toggle');
+    menu.addEventListener('click', function(){
+        var nav = document.querySelector('.main-menu');
+        nav.classList.toggle('active');
+        var navGamb = document.querySelector('.menu-toggle');
+        navGamb.classList.toggle('active');
+    });
+//tabs
+	// store tabs variable
+	var myTabs = document.querySelectorAll("ul.header__tabs > li");
+    function myTabClicks(tabClickEvent) {
+		for (var i = 0; i < myTabs.length; i++) {
+			myTabs[i].classList.remove("active");
+		}
+		var clickedTab = tabClickEvent.currentTarget;
+		clickedTab.classList.add("active");
+		tabClickEvent.preventDefault();
+		var myContentPanes = document.querySelectorAll(".tab-pane");
+		for (i = 0; i < myContentPanes.length; i++) {
+			myContentPanes[i].classList.remove("active");
+		}
+        var anchorReference = tabClickEvent.target;
+        console.log(anchorReference);
+        var activePaneId = anchorReference.getAttribute("href");
+        console.log(activePaneId);
+        var activePane = document.querySelector(activePaneId);
+        console.log(activePaneId);
+		activePane.classList.add("active");
+    }
+    for (i = 0; i < myTabs.length; i++) {
+		myTabs[i].addEventListener("click", myTabClicks)
+	}
+
+
+
+
+
+});
+
+
+
+
